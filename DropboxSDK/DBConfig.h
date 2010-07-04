63407fc144f94c6daecb0cfed960d3d105305c42
Brian Partridge <thelaughingman@gmail.com>
2010-07-04 18:19:50 -0400

    added Dropbbox SDK source

---
+++ DropboxSDK/DBConfig.h
@@ -0,0 +1,8 @@
+/* Set these to whatever you use to log in and such. */
+
+// the DB_USERNAME and DB_PASSWORD are only needed for testing purposes, normally these wouldn't go in your application
+NSString *DB_USERNAME = @"your@login.com";
+NSString *DB_PASSWORD = @"yourpassword";
+NSString *DB_CONSUMERKEY = @"yourconsumerkey";
+NSString *DB_CONSUMERSECRET = @"yoursecret";
+
